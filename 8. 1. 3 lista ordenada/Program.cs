51d94e175ddd66374df6210542533f7ff699430a
Melanie <butusmelanie@gmail.com>
2025-05-29 09:34:27 -0300

    guia8

---
+++ 8. 1. 3 lista ordenada/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace _8._1._3_lista_ordenada
+{
+    internal class Program
+    {
+        static string nombre0, nombre1, nombre2;
+        static int numerolibreta0, numerolibreta1, numerolibreta2;
+        static int orden, opcion;
+
+        static void Main(string[] args)
+        {
+            MostrarPantallaSolicitarMenu();
+        }
+
+        static void MostrarPantallaSolicitarMenu()
+        {
+            Console.WriteLine(@"1 - Registrar las notas de los tres alumnos
+            2 - Mostrar lista ordenada");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    MostrarPantallaSolicitarAlumnos();
+                    MostrarPantallaSolicitarMenu();
+                    break;
+                case 2:
+                    MostrarPantallaMostrarListaOrdenada();
+                    break;
+            }
+        }
+
+        static void MostrarPantallaSolicitarAlumnos()
+        {
+                Console.WriteLine("Ingrese nombre del alumno");
+                nombre0 = Console.ReadLine();
+                Console.WriteLine("Ingrese nota del alumno");
+                numerolibreta0=Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingrese nombre del alumno");
+                nombre1 = Console.ReadLine();
+                Console.WriteLine("Ingrese nota del alumno");
+                numerolibreta1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingrese nombre del alumno");
+                nombre2 = Console.ReadLine();
+                Console.WriteLine("Ingrese nota del alumno");
+                numerolibreta2 = Convert.ToInt32(Console.ReadLine());
+                
+            }
+
+        static void MostrarPantallaMostrarListaOrdenada()
+        {
+            if (numerolibreta0 > numerolibreta1)
+            {
+                if (numerolibreta0 > numerolibreta2)
+                {
+                    if (numerolibreta1 > numerolibreta2)
+                        Console.WriteLine($"{numerolibreta0}, {numerolibreta1}, {numerolibreta2}");
+                    else
+                        Console.WriteLine($"{numerolibreta0}, {numerolibreta2}, {numerolibreta1}");
+                }
+                else
+                {
+                    Console.WriteLine($"{numerolibreta2}, {numerolibreta0}, {numerolibreta1}");
+                }
+            }
+            else
+            {
+                if (numerolibreta1 > numerolibreta2)
+                {
+                    if (numerolibreta0 > numerolibreta2)
+                        Console.WriteLine($"{numerolibreta1}, {numerolibreta0}, {numerolibreta2}");
+                    else
+                        Console.WriteLine($"{numerolibreta1}, {numerolibreta2}, {numerolibreta0}");
+                }
+                else
+                {
+                    Console.WriteLine($"{numerolibreta2}, {numerolibreta1}, {numerolibreta0}");
+                }
+            }
+
+        }
+
+    }
+}
---
+++ prueba2/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace prueba
+{
+    internal class Program
+    {
+       
+        static int numero, contador, acumulador;
+        static int nummayor = 0, nummenor = 0;
+        static int[] numeros;
+        static int x = 0;
+        static double promedio;
+
+        static void Main(string[] args)
+        {
+            MostrarPantallaSolicitarOpcionMenu();
+            MainMenu();
+        }
+
+        static void MainMenu()
+        {
+            
+        }
+
+        static void MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\t  Menú \t");
+            Console.WriteLine("1 - Procesar un solo número \r\n" +
+                "2 - Procesar varios números\r\n" +
+                "3 - Mostrar máximo y mínimo.\r\n" +
+                "4 - Mostrar promedio.\r\n" +
+                "5 - Mostrar cantidad de números ingresados.\r\n" +
+                "6 - Reiniciar variables.\r\n"+
+                "7 salir\n");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine(" Procesar un solo número");
+                    MostrarPantallaSolicitarNumero();
+                    Console.ReadLine(); 
+                    MostrarPantallaSolicitarOpcionMenu();
+                    break;
+
+                case "2":
+                    Console.WriteLine("Procesar varios números");
+                    MostrarPantallaSolicitarVariosNumeros();
+                    Console.ReadLine(); 
+                    MostrarPantallaSolicitarOpcionMenu();
+                    break;
+
+                case "3":
+                    Console.WriteLine("Mostrar máximo y mínimo");
+                    MostrarPantallaMaximo();
+                    MostrarPantallaMinimo();
+                    Console.ReadLine();
+                    MostrarPantallaSolicitarOpcionMenu();
+                    break;
+
+                case "4":
+                    Console.WriteLine("Mostrar promedio");
+                    MostrarPantallaCalcularYMostrarPromedio();
+                    Console.ReadLine(); 
+                    MostrarPantallaSolicitarOpcionMenu();
+                    break;
+
+                case "5":
+                    Console.WriteLine(" Mostrar cantidad de números ingresados");
+                    MostrarPantallaCantidad();
+                    Console.ReadLine(); 
+                    MostrarPantallaSolicitarOpcionMenu();
+                    break;
+
+                case "6":
+                    Console.WriteLine("Reiniciar variables");
+                    MostrarIniciarVariables();
+                    Console.ReadLine(); 
+                    MostrarPantallaSolicitarOpcionMenu();
+                    break;
+                case "7":
+                    Console.WriteLine("salir");
+                    break;
+            }
+        }
+
+        static void MostrarIniciarVariables()
+        {
+            contador = 0;
+            acumulador = 0;
+        }
+
+        static void MostrarPantallaSolicitarNumero()
+        {
+            Console.WriteLine("Ingrese número");
+            numero = Convert.ToInt32(Console.ReadLine());
+            contador += 1;
+        }
+
+        static void MostrarPantallaSolicitarVariosNumeros()
+        {
+            Console.WriteLine("Ingrese cantidad de números a Ingresar");
+            x = Convert.ToInt32(Console.ReadLine());
+            numeros = new int[x];
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("Ingrese número");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+                contador += 1;
+            }
+        }
+
+        static void MostrarPantallaMaximo()
+        {
+            if (numero > nummayor)
+            {
+                nummayor = numero;
+            }
+            Console.WriteLine($"El maximo es {nummayor}");
+        }
+
+        static void MostrarPantallaMinimo()
+        {
+            if (numero < nummenor)
+            {
+                nummenor = numero;
+            }
+            Console.WriteLine($"El minimo es {nummenor}");
+        }
+
+        static void MostrarPantallaCalcularYMostrarPromedio()
+        {
+            promedio = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                acumulador += numeros[i];
+            }
+
+            promedio = (double)acumulador / numeros.Length;
+            Console.WriteLine($"El promedio es {promedio}");
+        }
+
+        static void MostrarPantallaCantidad()
+        {
+            if (numeros != null && numeros.Length > 0)
+            {
+                Console.WriteLine("La cantidad es: " + numeros.Length);
+            }
+            else
+            {
+                Console.WriteLine("No se han ingresado números.");
+            }
+        }
+    }
+}
